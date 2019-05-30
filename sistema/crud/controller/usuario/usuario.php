aad158508cca40269153d84f63a9d3e34242eed0
CViniciusS <47905742+CViniciusS@users.noreply.github.com>
2019-05-29 21:54:28 -0300

    CRUD - SALVAR - 29/05
    CRUD - SALVAR - 29/05

--- sistema/crud/controller/usuario/usuario.php
+++ sistema/crud/controller/usuario/usuario.php
@@ -1,16 +1,19 @@
-<!DOCTYPE html>
 <html>
 
 <head>
     <?php
-    require_once '/view/head.php';
+    require_once '../../view/head.php';
     ?>
 </head>
 
 <body>
-    <div class="container">
-        <div class="row justify-content-center bg-light">
-            <form action="" method="POST">
+    <?php
+    require_once '../../view/nav.php';
+    ?>
+
+<div class="container">
+<div class="row justify-content-center bg-light">
+            <form action="usuarioDAO.php" method="POST">
                 <div class="form-group">
                     <label>Usuário:</label>
                     <input type="text" class="form-control" name="usuario" id="usuario">
@@ -21,12 +24,12 @@
                     <input type="password" class="form-control" name="senha" id="senha">
                 </div>
                 <div class="form-group">
-                    <button type="submit" class="btn btn-primary" name="autenticar">Entrar</button>
-                    <a href="/sistema/index.html" class="btn btn-info">Voltar</a>
+                    <button type="submit" class="btn btn-primary" name="salvar">Salvar</button>
+                    
                 </div>
             </form>
         </div>
-    </div>
+</div>
 </body>
 
 </html>
---
+++ sistema/crud/menu.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <?php
+    require_once '/view/head.php';
+    ?>
+</head>
+
+    <body>
+
+    <?php
+    require_once '/view/nav.php';
+    ?>
+
+    </body>
+
+</html>
